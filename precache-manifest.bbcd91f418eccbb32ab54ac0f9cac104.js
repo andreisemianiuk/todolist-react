c2d3fa71bab97dc18312afa7d7ec348d5198be7f
andreisemianiuk <semeniukandrei677@gmail.com>
2021-03-11 12:32:04 +0300

    Updates

---
+++ precache-manifest.bbcd91f418eccbb32ab54ac0f9cac104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dff8c9c86a3bbbc863e4e2377b4a93",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "96d4527ac30ab9848f21",
+    "url": "/todolist-react/static/css/main.9ff5e910.chunk.css"
+  },
+  {
+    "revision": "b10d71a6e16197ae3644",
+    "url": "/todolist-react/static/js/2.932e9205.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist-react/static/js/2.932e9205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d4527ac30ab9848f21",
+    "url": "/todolist-react/static/js/main.39765346.chunk.js"
+  },
+  {
+    "revision": "5c812ffb49de4d91edbc",
+    "url": "/todolist-react/static/js/runtime-main.6525dce5.js"
+  }
+]);
